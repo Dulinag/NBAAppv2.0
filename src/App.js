c1ce88a6d454e2cbcc2d77ddcfc38bf880b86490
Bennett Kautz <92739107+bkautz0824@users.noreply.github.com>
2023-03-27 16:52:25 -0400

    player card update

--- src/App.js
+++ src/App.js
@@ -7,37 +7,43 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
-  const [players, setPlayers] = useState(playerNames);
+  const [players, setPlayers] = useState(playerStats);
   const [selectedPlayer, setSelectedPlayer] = useState(null);
 
   const handlePlayerClick = (player) => {
 
     setSelectedPlayer(player);
   }
-
+  console.log(playerStats)
   return (
     <div className="container">
       <h1 className="my-4">List of NBA Players</h1>
       
       <div className="row row-cols-2 row-cols-md-3 g-4">
-        {players.map((player, index) => {
-          const playerStat = playerStats.find(stat => stat.name === player);
-          const pointsPerGame = playerStat ? playerStat.pointsPerGame : null;
-
-          return (
+        {playerStats.map((player, index) => {
+          let playerInfo = player.data[0]
+          // console.log(player.data[0])
+        //  player.data[0] === undefined ? console.log(player) : console.log(player.data[0].first_name)
+          return ( 
+            <>
+            {playerInfo !== undefined ? 
             <div className="col" key={index}>
-
+             
               <div className="card" onClick={() => handlePlayerClick(player)}>
+             
                 <div className="card-body">
 
-                  <h5 className="card-title">{player}</h5>
-                  {pointsPerGame && <p className="card-text">Points per game: {pointsPerGame}</p>}
-                </div>
+                  <h5 className="card-title">{playerInfo.first_name + ' ' + playerInfo.last_name}</h5> 
+                   <p className="card-text">{playerInfo.team.full_name} </p>
 
-
-
-              </div>
+                </div> 
+               
+              
+                
+              </div>  
             </div>
+            : null}
+            </>
           );
         })}
       </div>
