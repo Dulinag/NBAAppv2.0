38ec1329095db00250e72e24ff5831569e9023be
Bennett Kautz <92739107+bkautz0824@users.noreply.github.com>
2023-03-27 17:21:28 -0400

    3/27 last commit of day

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import playerNames from './playerNames.json';
-
+import { useParams } from 'react-router-dom';
 import playerStats from './playerStats.json';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -11,10 +11,10 @@ function App() {
   const [selectedPlayer, setSelectedPlayer] = useState(null);
 
   const handlePlayerClick = (player) => {
-
+    console.log(player)
     setSelectedPlayer(player);
   }
-  console.log(playerStats)
+  // console.log(playerStats)
   return (
     <div className="container">
       <h1 className="my-4">List of NBA Players</h1>
@@ -27,19 +27,17 @@ function App() {
           return ( 
             <>
             {playerInfo !== undefined ? 
-            <div className="col" key={index}>
+            <div className="col" key={playerInfo.id}>
              
-              <div className="card" onClick={() => handlePlayerClick(player)}>
+              <div className="card" onClick={() => handlePlayerClick(playerInfo.id)}>
              
                 <div className="card-body">
 
                   <h5 className="card-title">{playerInfo.first_name + ' ' + playerInfo.last_name}</h5> 
-                   <p className="card-text">{playerInfo.team.full_name} </p>
+                  <p className="card-text">{playerInfo.team.full_name} </p>
 
                 </div> 
                
-              
-                
               </div>  
             </div>
             : null}
